fe527bc9c80ac085b5c26925626aae9c4ea58d3d
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2022-07-13 16:11:48 -0500

    updated grunt file to put timestamps in the compiled JS file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,7 @@
 var fs = require("fs");
+var version = "0.10.1";
 
-
-module.exports = function(grunt){
-  grunt.initConfig({
+var configObj = {
     "pkg": "package.json",
     "uglify": {
       "options": {
@@ -12,20 +11,45 @@ module.exports = function(grunt){
       },
       "my_target": {
         "files": {
-          "dist/keyri-0.10.1.min.js": ["js/AwesomeQR.js","js/EZCrypto.js","js/KeyriQR.js","js/SimpleSocket.js","js/main.js"]
+          "dist/XXX.min.js": ["js/AwesomeQR.js","js/EZCrypto.js","js/KeyriQR.js","js/SimpleSocket.js","js/main.js"]
         }
       }
     }
-  });
+  }
+  
+// GANK IT UP!
+configObj.uglify.my_target.files[`dist/keyri-${version}.min.js`] = configObj.uglify.my_target.files["dist/XXX.min.js"];
+delete configObj.uglify.my_target.files["dist/XXX.min.js"];
+
+console.log(configObj.uglify.my_target)
+
+
+module.exports = function(grunt){
+  
+  grunt.initConfig(configObj);
+  grunt.task.registerTask("timestamp","what", function(){
+    
+    var done = this.async();
+    setTimeout(function() {
+      console.log("WRITING!");
+      fs.appendFile(`dist/keyri-${version}.min.js`, `\n\n\n console.log("SCRIPT COMPILED: ${new Date().toString()} ");`, (err, data) => {
+            grunt.log.writeln('WROTE');
+            grunt.log.writeln(new Date())
+            done();
+      })
+      
+    }, 2000);
+  })
 
 
 
 
-    grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.registerTask("default", [
-        "uglify"
-    ]);
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask("default", [
+      "uglify",
+      "timestamp"
+  ]);
 
 
 
